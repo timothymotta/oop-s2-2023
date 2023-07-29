ca6d5545289c8510f70756729e9883bdc18cb643
Timothy Motta <140376053+timothymotta@users.noreply.github.com>
2023-07-29 23:15:30 +0930

    Update main-3-1.cpp

--- main-3-1.cpp
+++ main-3-1.cpp
@@ -3,19 +3,12 @@
 bool is_fanarray(int array[], int n);
 
 int main() {
-  int arr1[] = {1, 2, 3, 2, 1};
-  int n1 = sizeof(arr1) / sizeof(arr1[0]);
+  int arr[] = {1, 2, 3, 2, 1};
+  int n = sizeof(arr) / sizeof(arr[0]);
 
-  int arr2[] = {1, 3, 5, 4, 2};
-  int n2 = sizeof(arr2) / sizeof(arr2[0]);
+  bool result = is_fanarray(arr, n);
 
-  bool result1 = is_fanarray(arr1, n1);
-  bool result2 = is_fanarray(arr2, n2);
-
-  std::cout << "Array 1 is a fan array: " << std::boolalpha << result1
+  std::cout << "Array 1 is a fan array: " << std::boolalpha << result
             << std::endl;
-  std::cout << "Array 2 is a fan array: " << std::boolalpha << result2
-            << std::endl;
-
   return 0;
 }
