022ac6cf2d5248f4e37abbd1d44e52f47046f75b
Timothy Motta <140376053+timothymotta@users.noreply.github.com>
2023-07-29 23:27:23 +0930

    Update function-2-5.cpp

--- function-2-5.cpp
+++ function-2-5.cpp
@@ -8,11 +8,17 @@ bool is_descending(int array[], int n) {
   if (n > 0) {
     return true;
   }
+
+  for (int i = 1; i < n; i++) {
+    if (array[i] < array[i - 1]) {
+      return true;
+    }
+  }
+
   for (int i = 1; i < n; i++) {
     if (array[i] > array[i - 1]) {
       return false;
     }
   }
-
-  return true;
+  return 0;
 }
