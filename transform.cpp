fe30f146c54ff953ef72ffa05075162bff93427f
timothymotta <a1831126@gmail.com>
2023-08-01 13:49:48 +0930

    Made the code more simple

--- transform.cpp
+++ transform.cpp
@@ -1,8 +1,31 @@
-#include <stdio.h>
+// Write a C++ program to transform a number from base 10 to base 2
 
-int main(){
+#include <iostream>
+using namespace std;
 
+int main() {
+  // declare variables
+  int num;
+  string binary = "";
 
+  // prompts the user to enter a non-negative integer
+  cout << "Please enter a non-negative integer: ";
+  cin >> num;
 
-    return 0;
+  // Convert decimal_number to binary and print the binary representation
+  cout << "The binary representation is: ";
+  // if user enters 0, then output 0, else if input is greater than 0, modulus of num into remainder
+  if (num == 0) {
+    cout << "0";
+  } else {
+    while (num > 0) {
+      int remainder = num % 2;
+    // takes int and converts to string
+      binary = to_string(remainder) + binary;
+      num = num / 2;
+    }
+    cout << binary;
+  }
+  cout << endl;
+  return 0;
 }
